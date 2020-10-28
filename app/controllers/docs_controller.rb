6c86ba433921275dc505d37676b3c72133dcb1f3
Diego Paiva <diego.paiva@imobsoft.com.br>
2020-10-27 23:07:12 -0300

    crud form docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -23,13 +23,20 @@ class DocsController < ApplicationController
 
     end
 
-    def update
-    end
-
     def edit
     end
 
+    def update
+        if @doc.update(docs_params)
+            redirect_to @doc 
+        else 
+            render 'edit'
+        end
+    end
+
     def destroy
+        @doc.destroy
+            redirect_to docs.path 
     end
 
     private
